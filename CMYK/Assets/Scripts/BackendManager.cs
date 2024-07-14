8a0430394fa68ec48a31cc6278af7a7e3b0bb449
stayup24h <102606991+stayup24h@users.noreply.github.com>
2024-07-15 01:40:11 +0900

    :recycle: refactor: remake a project file

---
+++ CMYK/Assets/Scripts/BackendManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 뒤끝 SDK namespace 추가
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    void Start()
+    {
+        var bro = Backend.Initialize(true); // 뒤끝 초기화
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
+        }
+        else
+        {
+            Debug.LogError("초기화 실패 : " + bro); // 실패일 경우 statusCode 400대 에러 발생
+        }
+    }
+}
---
+++ CMYK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
